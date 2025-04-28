b6021e98bb051d2fa2ec00ccf3f9ac280d2f3696
OlgaRaukuts <raukuts.olga@gmail.com>
2025-04-28 21:31:10 +0200

    task was solved but with issues need to rethink tomorrow

--- credit-card-checker-starter/main.js
+++ credit-card-checker-starter/main.js
@@ -25,7 +25,100 @@ const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, inval
 
 // Add your functions below:
 
+function validateCred(array){
+  let sum = 0;
+    for (let i = array.length - 1; i >= 0; i--) {
+        let digit = array[i];
+        if ((array.length - i) % 2 === 0) {
+            digit *= 2;
+        
+            if (digit > 9) {
+                digit -= 9;
+            }
+        }
+        sum += digit;
+    }
+    return sum % 10 === 0;
+}
 
+
+console.log(validateCred(valid4));
+
+
+
+function findInvalidCards(arr){
+  const newArr = [];
+for( let i = 0; i < arr.length; i++){
+		
+		if(validateCred(arr[i]) === false) {
+			
+			newArr.push(arr[i])
+		}
+		return newArr;
+	}
+ }
+
+console.log(findInvalidCards(batch));
+
+const isInvalidCardCompanies = (arr) => {
+let invalidCardCompanies = [];
+for(let i = 0; i < arr.length; i++){
+  let companyName = '';
+switch(companyName){
+  case 3:{
+    companyName = 'Amex (American Express)';
+    break;
+  }
+  case 4:{
+    companyName = 'Visa';
+    break;
+  }
+  case 5:{
+    companyName = 'Mastercard';
+    break;
+  }
+  case 6:{
+    companyName = 'Discover';
+    break;
+  }
+  default: {
+    console.log('Company not found');
+    break;
+  }}
+  
+if (invalidCardCompanies.indexOf(companyName) === -1) {
+			invalidCardCompanies.push(companyName)
+		}
+		
+}
+return invalidCardCompanies;
+}
+
+isInvalidCardCompanies(batch);
+
+// To make it easier to test credit card numbers, create a function that accepts a string and converts it into an array of numbers like the initially provided arrays. (Check the hint for a helpful function)Create a function that will convert invalid numbers into valid numbers.
+
+
+/*
+
+1. Create a function, validateCred() that has a parameter of an array. The purpose of validateCred() is to return true when an array contains digits of a valid credit card number and false when it is invalid. This function should NOT mutate the values of the original array.
+
+To find out if a credit card number is valid or not, use the Luhn algorithm. Generally speaking, an algorithm is a series of steps that solve a problem — the Luhn algorithm is a series of mathematical calculations used to validate certain identification numbers, e.g. credit card numbers. The calculations in the Luhn algorithm can be broken down as the following steps:
+
+Starting from the farthest digit to the right, AKA the check digit, iterate to the left.
+As you iterate to the left, every other digit is doubled (the check digit is not doubled). If the number is greater than 9 after doubling, subtract 9 from its value.
+Sum up all the digits in the credit card number.
+If the sum modulo 10 is 0 (if the sum divided by 10 has a remainder of 0) then the number is valid, otherwise, it’s invalid.
+2. Create another function, findInvalidCards() that has one parameter for a nested array of credit card numbers. The role of findInvalidCards() is to check through the nested array for which numbers are invalid, and return another nested array of invalid cards.
+3.
+After finding all the invalid credit card numbers, it’s also necessary to identify the credit card companies that have possibly issued these faulty numbers. Create a function, idInvalidCardCompanies() that has one parameter for a nested array of invalid numbers and returns an array of companies.
+
+Currently, there are 4 accepted companies which each have unique first digits. The following table shows which digit is unique to which company:
+If the number doesn’t start with any of the numbers listed, print out a message like: “Company not found”.
+
+idInvalidCardCompanies() should return an array of companies that have mailed out cards with invalid numbers. This array should NOT contain duplicates, i.e. even if there are two invalid Visa cards, "Visa" should only appear once in the array.
+
+*/
 
 
 
