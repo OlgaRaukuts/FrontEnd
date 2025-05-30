f4fbd4d863c5b3d433c8e8b097c61c3fd422f7e9
OlgaRaukuts <raukuts.olga@gmail.com>
2025-05-30 20:27:55 +0200

    created Add button logic -> can add and remove tracks now

--- jammming/src/components/Track/Track.js
+++ jammming/src/components/Track/Track.js
@@ -1,13 +1,22 @@
  import React from 'react';
  import styles from './Track.module.css';
  
- function Track({track}){
+ function Track({track, onAdd, isRemoval}){
+
+    const handleAdd = () => {
+        if(onAdd){
+            onAdd(track);
+        }
+    }
 
 
             return (
     <div style={{ marginBottom: '1rem' }}>
       <p><strong>{track.name}</strong></p>
       <p>{track.artist} | {track.album}</p>
+      {!isRemoval && (
+        <button onClick={handleAdd}>➕ Add</button>
+      )}
     </div>
   );
 }
