a2e14af2bbbd5c27169c79f060d9722f4b925252
OlgaRaukuts <raukuts.olga@gmail.com>
2025-05-30 10:11:19 +0200

    hardcoded tracks for the Track component

--- jammming/src/components/Track.js
+++ jammming/src/components/Track.js
@@ -1,9 +1,23 @@
-import React, {useState} from 'react';
 
-function Track(props){
-    const [name, setName] = useState('');
-    
-
+export const tracks = [
+{
+    name: "Beliver",
+    id: 332323324,
+    artist: "Imagine Dragons",
+    album: "Beliver"
+},
+{
+    name: "In the end",
+    id: 3323433324,
+    artist: "Linkin Park",
+    album: "In the end"
+},
+{
+    name: "Immortal",
+    id: 33234113324,
+    artist: "Evanescence",
+    album: "Immortal"
 }
+]
 
-export default Track;
+
