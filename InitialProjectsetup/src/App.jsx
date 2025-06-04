16e93e08063a3f52a5e489dffd87ec37a44064e7
OlgaRaukuts <raukuts.olga@gmail.com>
2025-06-04 10:12:03 +0200

    no message

---
+++ InitialProjectsetup/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Main from './components/Main';
+import NavBar from "./components/NavBar";
+
+function App(){
+    return(
+        <>
+        <NavBar />
+        <Main />
+        
+        </>
+    );
+}
+
+
+
+
+
+export default App;
---
+++ InitialProjectsetup/src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./NavBar.module.css";
+
+function NavBar(){
+return(
+    <header>
+        <nav>
+        <img src="../react-logo.png" alt="React logo" className={styles.img}/>
+        <span>ReactFacts</span>
+        </nav>
+    </header>
+    
+)
+}
+
+
+export default NavBar;
+
+
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ * 
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
