a2e204f5a7af5c2fa7591c34950ad4f08b1ab2b6
OlgaRaukuts <raukuts.olga@gmail.com>
2025-06-05 14:22:18 +0200

    no message

--- traveljournal/src/App.js
+++ traveljournal/src/App.js
@@ -1,19 +1,44 @@
 
 import './App.css';
-import Header from './components/Header/Header';
-import Entry from './components/Entry/Entry';
+import Header from './components/Header/Header.jsx';
+import Entry from './components/Entry/Entry.jsx';
+import data from './data.js';
+
+
 
 function App() {
-  const etnaImage = 'https://images.unsplash.com/photo-1570978999428-f17f4761d1c3?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-  const fujiImage = 'https://plus.unsplash.com/premium_photo-1661964177687-57387c2cbd14?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
-
+  //const etnaImage = 'https://images.unsplash.com/photo-1570978999428-f17f4761d1c3?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+  //const fujiImage = 'https://plus.unsplash.com/premium_photo-1661964177687-57387c2cbd14?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+  const entryElements = data.map((entry) => {
+    return (
+        <Entry
+            src={entry.src}
+            alt={entry.alt}
+            title={entry.title}
+            country={entry.country}
+            googleMapsLink={entry.googleMapsLink}
+            dates={entry.dates}
+            text={entry.text} />
+    )
+})
+console.log(entryElements);
   return (
     <div>
       <header>
          <Header />
       </header>
       <main className="container">
-      <div>
+      {entryElements}
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
+
+/* 
+<div>
           <Entry 
               image={fujiImage}
               alt="mount fuji"
@@ -35,9 +60,5 @@ function App() {
               description="Mount Etna, or simply Etna, is an active stratovolcano on the east coast of Sicily, Italy, in the Metropolitan City of Catania, between the cities of Messina and Catania."
                 />
         </div>
-      </main>
-    </div>
-  );
-}
 
-export default App;
+*/
---
+++ traveljournal/src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        id: 1,
+        src: "https://plus.unsplash.com/premium_photo-1661964177687-57387c2cbd14?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        alt: "Mount Fuji",
+        title: "Mount Fuji",
+        country: "Japan",
+        googleMapsLink: "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttutps://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://images.unsplash.com/photo-1616128618694-96e9e896ecb7?q=80&w=3090&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Sydney Opera House"
+        },
+        title: "Sydney Opera House",
+        country: "Australia",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "27 May, 2021 - 8 Jun, 2021",
+        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://plus.unsplash.com/premium_photo-1677343210638-5d3ce6ddbf85?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            alt: "Geirangerfjord"
+        },
+        title: "Geirangerfjord",
+        country: "Norway",
+        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    },
+]
+
