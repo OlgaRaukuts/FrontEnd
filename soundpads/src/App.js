4fb9f7fae785e28ed6003d8a870bfd150162829d
OlgaRaukuts <raukuts.olga@gmail.com>
2025-06-10 20:35:15 +0200

    created a new project and Initialized state with the default value of the array pulled in from pads.js

---
+++ soundpads/src/App.js
@@ -0,0 +1,16 @@
+import pads from './pads';
+import React, {useState} from 'react';
+
+function App() {
+
+  const [pads, setPads] = useState([]);
+   return (
+        <main>
+            <div className="pad-container">
+                {/* <button>s go here */}
+            </div>
+        </main>
+    );
+}
+
+export default App;
