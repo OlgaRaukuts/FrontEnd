9f7adeec6e95d73be239693ab53fc0f3536e1cd9
OlgaRaukuts <raukuts.olga@gmail.com>
2025-06-19 11:42:14 +0200

    not found edited

--- AdoptADog/src/pages/detail/index.js
+++ AdoptADog/src/pages/detail/index.js
@@ -31,7 +31,7 @@ const PetDetailsPage = () => {
         <h3>Loading...</h3>
       ) : error ? (
         <div>
-          {/* Redirect to /pet-details-not-found if there was an error! */}
+          <Navigate to='/pet-details-not-found' />
         </div>
       ) : (
         <main>
