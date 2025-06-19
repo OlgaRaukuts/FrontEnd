68d8a986f3aa53009513016dbf6e70a297c7bdc7
OlgaRaukuts <raukuts.olga@gmail.com>
2025-06-19 10:42:23 +0200

    added appRouter

--- AdoptADog/src/App.js
+++ AdoptADog/src/App.js
@@ -4,14 +4,11 @@ import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom';
-// Add react-router-dom imports
 
-// create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = <Route path="/somepath" element={ <Root /> }> </Route>;
 
 function App() {
   return (
-    // replace below with a Router Provider
    <RouterProvider router={appRouter}/>
   );
 }
