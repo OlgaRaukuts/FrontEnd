d031ced862c82296584ee267bfae341ab7064be0
OlgaRaukuts <raukuts.olga@gmail.com>
2025-06-24 13:56:40 +0200

    new project added

---
+++ yola/src/App.js
@@ -0,0 +1,20 @@
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          YOLA
+        </p>
+        <a>
+          Post your videos
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
